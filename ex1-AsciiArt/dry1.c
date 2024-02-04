03ea48c329cd6d98ab1378570f2c8f3b6dd3a3e5
UriKH <urikh@icloud.com>
2024-02-04 19:19:25 +0200

    fix dry 1

---
+++ ex1-AsciiArt/dry1.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h> 
+
+char* stringDuplicator(char* string, int times){
+    assert(string);
+    assert(times > 0);
+    int length = strlen(string);
+    char* out = malloc(length * times + 1);
+    assert(out); // ask
+
+    for (int i = 0; i < times; i++){
+        strcpy(out + i * length, string);
+    }
+
+    out[times * length - 1] = '\0';
+    return out;
+}
