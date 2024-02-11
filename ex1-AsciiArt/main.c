98b1104fa9c06254475ff27e09c29cc3261c92f6
Uri Kasher Hitin <75569286+UriKH@users.noreply.github.com>
2024-02-11 12:36:00 +0200

    Update main.c

--- ex1-AsciiArt/main.c
+++ ex1-AsciiArt/main.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <assert.h>
 #include "RLEList.h"
 #include "AsciiArtTool.h"
 
@@ -57,17 +56,14 @@ void runCommand(char flag, char* sourcePath, char* targetPath){
 }
 
 int main(int argc, char* argv[]){
-    assert(argc == 4);
     if (argc != 4){
         return 0;
     }
 
-    assert(argv[1][0] == '-');
     if (argv[1][0] != '-'){
         return 0;
     }
 
-    assert(strlen(argv[1]) == 2);
     if (strlen(argv[1]) == 2){
         runCommand(argv[1][1], argv[2], argv[3]);
     }
