d09ba9ff9d7b49e7f0e9854bedb5ba18ffb569f2
Uri Kasher Hitin <75569286+UriKH@users.noreply.github.com>
2024-02-11 12:35:21 +0200

    Update RLEListTEST.c

--- ex1-AsciiArt/tests/RLEListTEST.c
+++ ex1-AsciiArt/tests/RLEListTEST.c
@@ -1,4 +1,29 @@
 #include "RLEList.h"
+#include <assert.h>
+#include <stdio.h>
+
+RLEList init_list(){
+    RLEList list = RLEListCreate();
+    for (int i = 0; i < 5; i++)
+    	RLEListAppend(list, 'a');
+    for (int i = 0; i < 12; i++)
+    	RLEListAppend(list, 'b');
+    for (int i = 0; i < 5; i++)
+    	RLEListAppend(list, ' ');
+    for (int i = 0; i < 5; i++)
+    	RLEListAppend(list, '@');
+    for (int i = 0; i < 1; i++)
+    	RLEListAppend(list, 'c');
+    return list;
+}
+
+void test_create_destroy(){
+    RLEList list = init_list();
+    assert<list != NULL>;
+   	RLEListDestroy(list);
+    assert<list == NULL>;
+}
+
 
 int main(){
     test_create();
