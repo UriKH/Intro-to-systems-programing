4a3cf55f965ee4778f89c2d0351bc8329e860ffb
EitanCo <intheocean.email@gmail.com>
2024-04-01 00:47:45 +0300

    added comments

--- ex4-mtmchkin/Cards/Encounter.h
+++ ex4-mtmchkin/Cards/Encounter.h
@@ -17,12 +17,48 @@ protected:
     int m_size = DEFAULT_SIZE;
 
 public:
+    /**
+     * Get the description of the card
+     * 
+     * @return the card's description
+    */
     string getDescription() const override;
+
+    /**
+     * Plays the encounter card for the given player
+     * 
+     * @param player the player that plays the encounter
+     * 
+     * @return if lost the encounter - the damage, if won - the loot
+    */
     int playCard(Player& player) const override;
 
+    /**
+     * Get the loot of the card
+     * 
+     * @return the card's loot
+    */
     int getLoot() const;
+
+    /**
+     * Get the power of the card
+     * 
+     * @return the card's power
+    */
     int getPower() const;
+
+    /**
+     * Get the damage of the card
+     * 
+     * @return the card's damage
+    */
     int getDamage() const;
+
+    /**
+     * Get the name of the card
+     * 
+     * @return the card's name
+    */
     virtual std::string getName() const override;
 
     Encounter(const string name, const int power, const int loot, const int damage, const int size) :
--- ex4-mtmchkin/Cards/Event.h
+++ ex4-mtmchkin/Cards/Event.h
@@ -11,7 +11,18 @@ protected:
 public:
     Event(const std::string&);
 
+    /**
+     * Get the description of the card
+     * 
+     * @return the card's description
+    */
     string getDescription() const override;
+
+    /**
+     * Get the name of the card
+     * 
+     * @return the card's name
+    */
     std::string getName() const override;
 };
 
@@ -19,6 +30,13 @@ class SolarEclipse : public Event{
 public:
     SolarEclipse() : Event("SolarEclipse"){}
 
+    /**
+     * Plays the solar eclipse card for the given player
+     * 
+     * @param player the player that plays the card
+     * 
+     * @return the change in the player's force
+    */
     int playCard(Player& player) const override{
         return player.getJob().applySolarEclipse(player);
     }
@@ -28,6 +46,13 @@ class PotionsMerchant : public Event{
 public:
     PotionsMerchant() : Event("PotionsMerchant"){}
 
+    /**
+     * Plays the Potions Merchant card for the give player
+     * 
+     * @param player the player that plays the card
+     * 
+     * @return the amount of potions bought
+    */
     int playCard(Player& player) const override{
         return player.getJob().applyPotionsMerchant(player);
     }
