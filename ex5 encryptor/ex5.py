60144dc5189f4a5d008851d6a7971da9d0c0fb59
UriKH <urikh@icloud.com>
2024-04-05 17:33:09 +0300

    passed all tests ex5 final

--- ex5 encryptor/ex5.py
+++ ex5 encryptor/ex5.py
@@ -107,7 +107,7 @@ def loadEncryptionSystem(dir_path, plaintext_suffix):
     with open(os.path.join(dir_path, 'config.json'), 'r') as jsonFile:
         configurations = json.load(jsonFile)
 
-        configurations['encrypt'] = bool(configurations['encrypt'])
+        configurations['encrypt'] = configurations['encrypt'] == 'True'
         if configurations['type'] == "Vigenere":
             configurations['type'] = VigenereCipher(configurations['key'])
         else:
@@ -117,7 +117,7 @@ def loadEncryptionSystem(dir_path, plaintext_suffix):
         for filename in os.listdir(dir_path):
             current_file_path = os.path.join(dir_path, filename)
             root, extension = os.path.splitext(current_file_path)
-            extension = extension.split('.')[-1]
+            extension = extension[1:]
 
             if not ((extension == plaintext_suffix and configurations['encrypt'])
                     or (extension == 'enc' and not configurations['encrypt'])):
@@ -135,3 +135,4 @@ def loadEncryptionSystem(dir_path, plaintext_suffix):
 
                 with open(new_file_path, 'w') as new_file:
                     new_file.write(data)
+
